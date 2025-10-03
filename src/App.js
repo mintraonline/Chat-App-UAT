a241e72645079d4a076b7ae34135fe3f0a7f48a2
inshal <inshal.ansari@neosoftmail.com>
2025-10-03 18:33:15 +0530

    Changes Reverted

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <AuthContextProvider>
-      <BrowserRouter basename="/Chat-App">
+      <BrowserRouter basename="/Chat-App-UAT">
         <Routes>
           <Route
             path="/"
